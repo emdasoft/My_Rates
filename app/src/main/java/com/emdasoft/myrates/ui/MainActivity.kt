bcb298a9f5922a434deb70e69493274d44b20f17
GesserWy <emdasoftware@gmail.com>
2022-10-19 23:13:29 +0300

    add_design

--- app/src/main/java/com/emdasoft/myrates/ui/MainActivity.kt
+++ app/src/main/java/com/emdasoft/myrates/ui/MainActivity.kt
@@ -3,8 +3,11 @@ package com.emdasoft.myrates.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
 import com.emdasoft.myrates.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,13 +22,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
-//        recycleSetup()
-
         viewPagerSetup()
     }
 
-    private fun viewPagerSetup(){
-        binding.viewPager2.adapter = MainAdapter()
+    private fun viewPagerSetup() {
+        binding.apply {
+            viewPager2.adapter = MainAdapter()
+            TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
+                //Some implementation...
+            }.attach()
+            viewPager2.clipToPadding = false
+            viewPager2.clipChildren = false
+            viewPager2.setPadding(128, 16, 128, 16)
+            viewPager2.offscreenPageLimit = 3
+            viewPager2.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+
+            val compositePageTransformer = CompositePageTransformer()
+            compositePageTransformer.addTransformer(MarginPageTransformer(30))
+            compositePageTransformer.addTransformer { page, position ->
+                val r = 1 - kotlin.math.abs(position)
+                page.scaleY = 0.8f + r * 0.1f
+            }
+            viewPager2.setPageTransformer(compositePageTransformer)
+
+        }
 
         viewModel.getRates()
 
@@ -34,23 +54,8 @@ class MainActivity : AppCompatActivity() {
             list.body()?.rates?.let { (binding.viewPager2.adapter as MainAdapter).setList(it) }
 
         }
-    }
 
-//    private fun recycleSetup() {
-//
-//        binding.rvRates.layoutManager = LinearLayoutManager(this)
-//        binding.rvRates.adapter = MainAdapter()
-//
-//        viewModel.getRates()
-//
-//        viewModel.rateList.observe(this) { list ->
-//
-//            list.body()?.rates?.let { (binding.rvRates.adapter as MainAdapter).setList(it) }
-//
-//        }
-//
-//
-//    }
+    }
 
 
 }
